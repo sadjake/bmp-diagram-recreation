c0898435029db4df159e059fa964da7ee66a9e26
Jake Choi <59670379+sadjake@users.noreply.github.com>
2023-08-06 16:34:25 -0700

    Update Gasket.cpp

--- Gasket.cpp
+++ Gasket.cpp
@@ -6,12 +6,14 @@ double GASKET_SIDE_TO_HEIGHT = 0.86666666;
 // Default constructor
 Gasket::Gasket()
 {
+	// initialize values
 	sideLength = 100;
 	xLocation = 0;
 	yLocation = 0;
 	iterations = 0;
 	gasketColour = Colour(0, 0, 0, 255);
 	lenSubTriList = 1;
+
 	listOfSubTriangles = new Triangle[lenSubTriList];
 	listOfSubTriangles[0] = Triangle(xLocation, yLocation, sideLength, gasketColour);
 }
@@ -19,13 +21,13 @@ Gasket::Gasket()
 // Constructor with all argumements
 Gasket::Gasket(int iterationsValue, int xLocationValue, int yLocationValue, int sideLengthValue, Colour gasketColourValue)
 {
+	// Initialize values based on constructor arguments
 	iterations = iterationsValue;
 	xLocation = xLocationValue;
 	yLocation = yLocationValue;
 	sideLength = sideLengthValue;
 
-	if (!(xLocation >= 0 && xLocation < pageWidth && yLocation >= 0 && yLocation < pageLength))
-	{
+	if (!(xLocation >= 0 && xLocation < pageWidth && yLocation >= 0 && yLocation < pageLength)) {
 		//cout << "TRIANGLE BOTH xLocation AND yLocation OFF THE PAGE: plotted " << triangColour << endl;
 		cout << "x position of Gasket object is off the page" << endl;
 		cout << "x position and y position have been reset to 0" << endl;
@@ -34,8 +36,7 @@ Gasket::Gasket(int iterationsValue, int xLocationValue, int yLocationValue, int 
 	}
 
 	//xLocation
-	if (xLocation + sideLength > pageWidth)
-	{
+	if (xLocation + sideLength > pageWidth) {
 		int temp = sideLength - (xLocation + sideLength - pageWidth);
 		cout << "scaling in x" << endl;
 		cout << "sideLength was " << sideLength << " sideLength now is " << temp << endl;
@@ -43,20 +44,20 @@ Gasket::Gasket(int iterationsValue, int xLocationValue, int yLocationValue, int 
 	}
 
 	//yLocation
-	if (yLocation + GASKET_SIDE_TO_HEIGHT * sideLength > pageLength)
-	{
+	if (yLocation + GASKET_SIDE_TO_HEIGHT * sideLength > pageLength) {
 		int temp = sideLength - (1 / GASKET_SIDE_TO_HEIGHT * (yLocation + GASKET_SIDE_TO_HEIGHT * sideLength - pageLength));
 		cout << "scaling in y" << endl;
 		cout << "sideLength was " << sideLength << " sideLength now is " << temp << endl;
 		sideLength = temp;
 	}
 
+	// Initialize other attributes
 	gasketColour = gasketColourValue;
 	lenSubTriList = static_cast<int> (pow(3, iterations));
 	listOfSubTriangles = new Triangle[lenSubTriList];
 	listOfSubTriangles[0] = Triangle(xLocation, yLocation, sideLength, gasketColour);
-	for (int i = 1; i <= iterations; i++)
-	{
+	
+	for (int i = 1; i <= iterations; i++) {
 		GasketDivide(listOfSubTriangles, lenSubTriList / 3);
 	}
 }
@@ -72,8 +73,8 @@ Gasket::Gasket(const Gasket& toBeCopied)
 	lenSubTriList = toBeCopied.lenSubTriList;
 	listOfSubTriangles = new Triangle[lenSubTriList];
 	listOfSubTriangles[0] = Triangle(xLocation, yLocation, sideLength, gasketColour);
-	for (int i = 0; i < lenSubTriList; i++)
-	{
+	
+	for (int i = 0; i < lenSubTriList; i++) {
 		listOfSubTriangles[i] = toBeCopied.listOfSubTriangles[i];
 	}
 }
@@ -86,15 +87,13 @@ Gasket::~Gasket()
 
 void Gasket::GasketDivide(Triangle* listOfSubTriangles, int trianglesIn)
 {
-	if (trianglesIn <= 0 || trianglesIn >= static_cast<int>(pow(3, iterations)))
-	{
+	if (trianglesIn <= 0 || trianglesIn >= static_cast<int>(pow(3, iterations))) {
 		cerr << " illegal number of triangle in Gasket Divide";
 		exit(1);
 	}
-	else
-	{
-		for (int k = trianglesIn - 1; k >= 0; k--)
-		{
+		
+	else {
+		for (int k = trianglesIn - 1; k >= 0; k--) {
 			listOfSubTriangles[k].TriangleDivide(&listOfSubTriangles[k * 3]);
 		}
 	}
@@ -143,8 +142,7 @@ void Gasket::SetXLocation(const int xLocationValue)
 {
 	xLocation = xLocationValue;
 
-	if (!(xLocation >= 0 && xLocation < pageWidth && yLocation >= 0 && yLocation < pageLength))
-	{
+	if (!(xLocation >= 0 && xLocation < pageWidth && yLocation >= 0 && yLocation < pageLength)) {
 		//cout << "TRIANGLE BOTH xLocation AND yLocation OFF THE PAGE: plotted " << triangColour << endl;
 		cout << "x position = " << xLocation << " and / or y position = " << yLocation << " of Triangle object is off the page" << endl;
 		xLocation = 0;
@@ -153,25 +151,21 @@ void Gasket::SetXLocation(const int xLocationValue)
 	}
 
 	//xLocation
-	if (xLocation + sideLength > pageWidth)
-	{
+	if (xLocation + sideLength > pageWidth) {
 		int temp = sideLength - (xLocation + sideLength - pageWidth);
 		sideLength = temp;
 	}
 
 	listOfSubTriangles[0] = Triangle(xLocation, yLocation, sideLength, gasketColour);
-	for (int k = 1; k <= iterations; k++)
-	{
+	for (int k = 1; k <= iterations; k++) {
 		GasketDivide(listOfSubTriangles, lenSubTriList / 3);
 	}
 }
 
-void Gasket::SetYLocation(const int yLocationValue)
-{
+void Gasket::SetYLocation(const int yLocationValue) {
 	yLocation = yLocationValue;
 
-	if (!(xLocation >= 0 && xLocation < pageWidth && yLocation >= 0 && yLocation < pageLength))
-	{
+	if (!(xLocation >= 0 && xLocation < pageWidth && yLocation >= 0 && yLocation < pageLength)) {
 		//cout << "TRIANGLE BOTH xLocation AND yLocation OFF THE PAGE: plotted " << triangColour << endl;
 		cout << "x position = " << xLocation << " and / or y position = " << yLocation << " of Triangle object is off the page" << endl;
 		xLocation = 0;
@@ -180,15 +174,14 @@ void Gasket::SetYLocation(const int yLocationValue)
 	}
 
 	//yLocation
-	if (yLocation + GASKET_SIDE_TO_HEIGHT * sideLength > pageLength)
-	{
+	if (yLocation + GASKET_SIDE_TO_HEIGHT * sideLength > pageLength) {
 		int temp = sideLength - (1 / GASKET_SIDE_TO_HEIGHT * (yLocation + GASKET_SIDE_TO_HEIGHT * sideLength - pageLength));
 		sideLength = temp;
 	}
 
 	listOfSubTriangles[0] = Triangle(xLocation, yLocation, sideLength, gasketColour);
-	for (int k = 1; k <= iterations; k++)
-	{
+	
+	for (int k = 1; k <= iterations; k++) {
 		GasketDivide(listOfSubTriangles, lenSubTriList / 3);
 	}
 }
@@ -197,8 +190,8 @@ void Gasket::SetGasketColour(const Colour gasketColorValue)
 {
 	gasketColour = gasketColorValue;
 	listOfSubTriangles[0] = Triangle(xLocation, yLocation, sideLength, gasketColour);
-	for (int k = 1; k <= iterations; k++)
-	{
+	
+	for (int k = 1; k <= iterations; k++) {
 		GasketDivide(listOfSubTriangles, lenSubTriList / 3);
 	}
 }
@@ -211,8 +204,8 @@ void Gasket::SetIterations(const int iterationsValue)
 	listOfSubTriangles = NULL;
 	listOfSubTriangles = new Triangle[lenSubTriList];
 	listOfSubTriangles[0] = Triangle(xLocation, yLocation, sideLength, gasketColour);
-	for (int k = 1; k <= iterations; k++)
-	{
+	
+	for (int k = 1; k <= iterations; k++) {
 		GasketDivide(listOfSubTriangles, lenSubTriList / 3);
 	}
 }
@@ -222,37 +215,36 @@ void Gasket::SetSideLength(const int sideLengthValue)
 	int height = 0;
 	sideLength = sideLengthValue;
 
+	if (sideLength < 1) {
+		sideLength = 1;
+	}
 	
-
-	if (sideLength < 1)  sideLength = 1;
-	if (xLocation + sideLength > pageWidth)
-	{
+	if (xLocation + sideLength > pageWidth) {
 		sideLength = pageWidth - xLocation;
 	}
 
 	height = static_cast<int> (sideLength * GASKET_SIDE_TO_HEIGHT);
 	
-	if (yLocation + height > pageLength)
-	{
+	if (yLocation + height > pageLength) {
 		sideLength = static_cast<int>((pageLength - yLocation) / GASKET_SIDE_TO_HEIGHT);
 	}
 
 	listOfSubTriangles[0] = Triangle(xLocation, yLocation, sideLength, gasketColour);
 	
-	for (int k = 1; k <= iterations; k++)
-	{
+	for (int k = 1; k <= iterations; k++) {
 		GasketDivide(listOfSubTriangles, lenSubTriList / 3);
 	}
 }
 
+// display all the sub-triangles in the gasket
 void Gasket::GasketDisplay(BMP& myImageValue, int thisIter, string theOutputFile)
 {
-	for (int i = 0; i < lenSubTriList; i++)
-	{
+	for (int i = 0; i < lenSubTriList; i++) {
 		listOfSubTriangles[i].TriangleDisplay(myImageValue, theOutputFile);
 	}
 }
 
+// print gasket attributes
 void Gasket::GasketPrint() {
 	cout << "iterations: " << iterations << endl;
 	cout << "xLocation: " << xLocation << endl;
@@ -261,8 +253,7 @@ void Gasket::GasketPrint() {
 	cout << "gasketColour: " << gasketColour << endl;
 	cout << "lenSubTriList: " << lenSubTriList << "\n" << endl;
 
-	for (int k = lenSubTriList - 1; k >= 0; k--)
-	{
+	for (int k = lenSubTriList - 1; k >= 0; k--) {
 		listOfSubTriangles[k].TrianglePrint();
 	}
 }
